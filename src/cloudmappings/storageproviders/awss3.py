9ddf67880931fa92d0f17115140a0e302c8ad56e
Lucas <lucas.sargent@outlook.com>
2021-05-16 06:46:51 +0000

    s3 passing singleton tests

--- src/cloudmappings/storageproviders/awss3.py
+++ src/cloudmappings/storageproviders/awss3.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 from uuid import uuid4
 
@@ -13,20 +14,44 @@ class AWSS3Provider(StorageProvider):
     def __init__(
         self,
         bucket_name: str,
+        silence_warning: bool = False,
     ) -> None:
         self._client = boto3.client("s3")
         self._bucket_name = bucket_name
+        if not silence_warning:
+            logging.warning(
+                (
+                    "AWS S3 does not support server-side atomic requests, it is not recommended for concurrent use.",
+                    "Consider using another provider such as Azure or GCP if you need concurrent access.",
+                    "You may silence this warning with silence_warning=True",
+                )
+            )
 
     def safe_name(self) -> str:
         return "CloudStorageProvider=AWSS3," f"BucketName={self._bucket_name}"
 
     def create_if_not_exists(self):
+        already_exists = False
         bucket = boto3.resource("s3").Bucket(self._bucket_name)
-        try:
+
+        # Note: There is a race condition here:
+        # If bucket is created after the creation_date is fetched but before the create call succeeds.
+        # Currently, bucket.creation_date appears to be the only way to check if a bucket exists in s3,
+        # and the create*() series of methods silently return success even if the bucket already exists.
+        # TODO: Monitor S3 API to see if a parameter to support atomic creations is added.
+        if bucket.creation_date is not None:
+            already_exists = True
+            existing_versioning = self._client.get_bucket_versioning(Bucket=self._bucket_name)
+            if existing_versioning["Status"] != "Enabled":
+                raise ValueError(
+                    "Found existing bucket with Versioning disabled. Enable versioning or specify a non-existing bucket name."
+                )
+        else:
             bucket.create()
-        except bucket.meta.client.exceptions.BucketAlreadyExists:
-            return True
-        return False
+            bucket.Versioning().put(
+                VersioningConfiguration={"Status": "Enabled"},
+            )
+        return already_exists
 
     def _get_body_etag_version_id_if_exists(self, key: str) -> Dict:
         try:
@@ -34,12 +59,13 @@ class AWSS3Provider(StorageProvider):
                 Bucket=self._bucket_name,
                 Key=key,
             )
-            return response["Body"], response["Metadata"][_metadata_etag_key], response["VersionId"]
-        except self._client.meta.client.exceptions.NoSuchKey:
             return (
-                None,
-                None,
+                response["Body"],
+                response["Metadata"][_metadata_etag_key],
+                response["VersionId"],
             )
+        except self._client.exceptions.NoSuchKey:
+            return (None, None, None)
 
     def download_data(self, key: str, etag: str) -> bytes:
         body, existing_etag, _ = self._get_body_etag_version_id_if_exists(key)
@@ -48,11 +74,15 @@ class AWSS3Provider(StorageProvider):
         return body.read()
 
     def upload_data(self, key: str, etag: str, data: bytes) -> str:
-        body, existing_etag, _ = self._get_body_etag_version_id_if_exists(key)
-        if body is not None and (etag is None or etag != existing_etag):
+        _, existing_etag, _ = self._get_body_etag_version_id_if_exists(key)
+        if etag != existing_etag:
             self.raise_key_sync_error(key=key, etag=etag)
-
-        new_etag = uuid4()
+        # Note: There is a race condition here:
+        # If blob is changed after the etag is fetched but before the put_object call succeeds.
+        # Currently, the S3 API does not appear to support any parameters that would enable server-side
+        # conflict checking.
+        # TODO: Monitor S3 API to see if a parameter to support atomic requests is added.
+        new_etag = str(uuid4())
         self._client.put_object(
             Bucket=self._bucket_name,
             Key=key,
@@ -67,6 +97,11 @@ class AWSS3Provider(StorageProvider):
         body, existing_etag, version_id = self._get_body_etag_version_id_if_exists(key)
         if body is None or etag != existing_etag:
             self.raise_key_sync_error(key=key, etag=etag)
+        # Note: There is a race condition here:
+        # If blob is changed after the etag is fetched but before the delete_object call succeeds.
+        # Currently, the S3 API does not appear to support any parameters that would enable server-side
+        # conflict checking.
+        # TODO: Monitor S3 API to see if a parameter to support atomic requests is added.
         self._client.delete_object(
             Bucket=self._bucket_name,
             Key=key,
@@ -75,9 +110,7 @@ class AWSS3Provider(StorageProvider):
 
     def list_keys_and_etags(self, key_prefix: str) -> Dict[str, str]:
         bucket = boto3.resource("s3").Bucket(self._bucket_name)
-        return {
-            o.key: o.Object.metadata[_metadata_etag_key]
-            for o in bucket.objects.filter(
-                Prefix=key_prefix,
-            )
-        }
+        kwargs = {}
+        if key_prefix is not None:
+            kwargs["Prefix"] = key_prefix
+        return {o.key: o.Object().metadata[_metadata_etag_key] for o in bucket.objects.filter(**kwargs)}
