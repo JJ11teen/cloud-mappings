4fa66323158d4b947e3efd7b3e9fbb4784e86864
Lucas <lucas.sargent@outlook.com>
2021-05-16 05:43:24 +0000

    update provider tests to use test_id prefix too

--- tests/tests/1_storageproviders.py
+++ tests/tests/1_storageproviders.py
@@ -12,36 +12,46 @@ class StorageProviderTests:
         assert storage_provider.create_if_not_exists() == False
         assert storage_provider.create_if_not_exists() == True
 
-    def test_data_is_stored(self, storage_provider):
-        etag = storage_provider.upload_data("key1", None, b"data")
-        data = storage_provider.download_data("key1", etag)
+    def test_data_is_stored(self, storage_provider, test_id):
+        key = test_id + "-data-store-test"
+
+        etag = storage_provider.upload_data(key, None, b"data")
+        data = storage_provider.download_data(key, etag)
+
         assert data == b"data"
 
-    def test_keys_and_etags_are_listed(self, storage_provider):
-        etag_3 = storage_provider.upload_data("key3", None, b"data")
-        etag_4 = storage_provider.upload_data("key4", None, b"data")
+    def test_keys_and_etags_are_listed(self, storage_provider, test_id):
+        key_1 = test_id + "-keys-and-etags-list-test-1"
+        key_2 = test_id + "-keys-and-etags-list-test-2"
+
+        etag_1 = storage_provider.upload_data(key_1, None, b"data")
+        etag_2 = storage_provider.upload_data(key_2, None, b"data")
         keys_and_etags = storage_provider.list_keys_and_etags(None)
 
-        assert "key3" in keys_and_etags
-        assert "key4" in keys_and_etags
-        assert etag_3 == keys_and_etags["key3"]
-        assert etag_4 == keys_and_etags["key4"]
+        assert key_1 in keys_and_etags
+        assert key_2 in keys_and_etags
+        assert etag_1 == keys_and_etags[key_1]
+        assert etag_2 == keys_and_etags[key_2]
 
-    def test_keys_are_deleted(self, storage_provider):
-        etag = storage_provider.upload_data("key5", None, b"data")
-        storage_provider.delete_data("key5", etag)
+    def test_keys_are_deleted(self, storage_provider, test_id):
+        key = test_id + "-keys-deleted-test"
 
-        cloud_key_list = storage_provider.list_keys_and_etags("key5")
-        assert "key5" not in cloud_key_list
+        etag = storage_provider.upload_data(key, None, b"data")
+        storage_provider.delete_data(key, etag)
 
-    def test_etags_are_enforced(self, storage_provider):
-        with pytest.raises(KeySyncError):
-            storage_provider.upload_data("key6", "etag-when-none-existing", b"data")
+        cloud_key_list = storage_provider.list_keys_and_etags(key)
+        assert key not in cloud_key_list
 
-        storage_provider.upload_data("key6", None, b"0")
+    def test_etags_are_enforced(self, storage_provider, test_id):
+        key = test_id + "etags-enforced-test"
+
         with pytest.raises(KeySyncError):
-            storage_provider.download_data("key6", "bad-etag")
+            storage_provider.upload_data(key, "etag-when-none-existing", b"data")
+
+        storage_provider.upload_data(key, None, b"0")
         with pytest.raises(KeySyncError):
-            storage_provider.upload_data("key6", "bad-etag", b"data")
+            storage_provider.download_data(key, "bad-etag")
         with pytest.raises(KeySyncError):
-            storage_provider.delete_data("key6", "bad-etag")
+            storage_provider.upload_data(key, "bad-etag", b"data")
+        with pytest.raises(KeySyncError):
+            storage_provider.delete_data(key, "bad-etag")
