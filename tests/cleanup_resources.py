fbd53602c6b383baddbd17cc2a9b9ee316e13370
Lucas <lucas.sargent@outlook.com>
2021-05-24 09:49:43 +0000

    add utility to clean out resources created by tests

---
+++ tests/cleanup_resources.py
@@ -0,0 +1,26 @@
+# This is a helper utility to remove cloud resources created by running tests.
+# Azure containers are not cleaned up as they can (and should) be set to auto-delete after a week.
+
+if __name__ == "__main__":
+    import boto3
+    from google.cloud import storage
+
+    client = boto3.client("s3")
+    s3 = boto3.resource("s3")
+
+    buckets = client.list_buckets()
+
+    for bucket in buckets["Buckets"]:
+        s3_bucket = s3.Bucket(bucket["Name"])
+        s3_bucket.objects.all().delete()
+        s3_bucket.object_versions.delete()
+        s3_bucket.delete()
+        print(f"Deleted s3 bucket {bucket['Name']}")
+
+    storage_client = storage.Client()
+    buckets = storage_client.list_buckets()
+    for bucket in buckets:
+        bucket.delete(force=True)
+        print(f"Deleted gcp bucket {bucket.name}")
+
+    print(f"Not deleting Azure containers")
