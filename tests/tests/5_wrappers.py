378d85ad83ba0cb8ca3f616e9abd9eba0299d4e4
Lucas <lucas.sargent@outlook.com>
2021-07-14 08:31:10 +0000

    add test for mapping wrapper objects

---
+++ tests/tests/5_wrappers.py
@@ -0,0 +1,79 @@
+import pytest
+
+from cloudmappings import AzureBlobMapping, AzureTableMapping, GoogleCloudStorageMapping, AWSS3Mapping
+
+
+class WrapperTests:
+    def test_azure_blob_mapping(self, azure_blob_storage_account_url, test_container_name):
+        cm = AzureBlobMapping(
+            account_url=azure_blob_storage_account_url,
+            container_name=test_container_name,
+            sync_initially=False,
+            read_blindly=True,
+        )
+        assert len(cm.etags) == 0
+        assert cm.get_read_blindly() == True
+
+        cm = AzureBlobMapping(
+            account_url=azure_blob_storage_account_url,
+            container_name=test_container_name,
+        )
+        assert len(cm.etags) > 0
+        assert cm.get_read_blindly() == False
+        cm.set_read_blindly(True)
+        assert cm.get_read_blindly() == True
+
+    def test_azure_table_mapping(self, azure_table_storage_connection_string, test_container_name):
+        cm = AzureTableMapping(
+            connection_string=azure_table_storage_connection_string,
+            table_name=test_container_name,
+            sync_initially=False,
+            read_blindly=True,
+        )
+        assert len(cm.etags) == 0
+        assert cm.get_read_blindly() == True
+
+        cm = AzureTableMapping(
+            connection_string=azure_table_storage_connection_string,
+            table_name=test_container_name,
+        )
+        assert len(cm.etags) > 0
+        assert cm.get_read_blindly() == False
+        cm.set_read_blindly(True)
+        assert cm.get_read_blindly() == True
+
+    def test_gcp_storage_mapping(self, gcp_storage_project, test_container_name):
+        cm = GoogleCloudStorageMapping(
+            project=gcp_storage_project,
+            bucket_name=test_container_name,
+            sync_initially=False,
+            read_blindly=True,
+        )
+        assert len(cm.etags) == 0
+        assert cm.get_read_blindly() == True
+
+        cm = GoogleCloudStorageMapping(
+            project=gcp_storage_project,
+            bucket_name=test_container_name,
+        )
+        assert len(cm.etags) > 0
+        assert cm.get_read_blindly() == False
+        cm.set_read_blindly(True)
+        assert cm.get_read_blindly() == True
+
+    def test_aws_s3_mapping(self, test_container_name):
+        cm = AWSS3Mapping(
+            bucket_name=test_container_name,
+            sync_initially=False,
+            read_blindly=True,
+        )
+        assert len(cm.etags) == 0
+        assert cm.get_read_blindly() == True
+
+        cm = AWSS3Mapping(
+            bucket_name=test_container_name,
+        )
+        assert len(cm.etags) > 0
+        assert cm.get_read_blindly() == False
+        cm.set_read_blindly(True)
+        assert cm.get_read_blindly() == True
